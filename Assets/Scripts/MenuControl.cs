57b6100470971fd78e3fcd23a454646699c6952f
mkr64294 <mark-rice@bethel.edu>
2020-04-13 10:01:43 -0500

    More progress
    Buttons are all clickable. Still trying to work on editing text.

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MenuControl : MonoBehaviour
 {
     public GameObject MenuObject;
+    public GameObject MenuObject2;
+    public GameObject MenuObject3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +22,19 @@ public class MenuControl : MonoBehaviour
 
     public void OpenMenu()
     {
+
         MenuObject.SetActive(true);
+           
     }
 
     public void CloseMenu()
     {
         MenuObject.SetActive(false);
+        if (MenuObject2 != null)
+        {
+            MenuObject2.SetActive(false);
+            if (MenuObject3 != null)
+                MenuObject3.SetActive(false);
+        }
     }
 }
