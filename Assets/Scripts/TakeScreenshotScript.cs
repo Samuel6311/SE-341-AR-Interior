9bb50a1413d414454d44eb7fb439f409b91b38e7
Hissamuddin Shaikh <57640329+HissamShk@users.noreply.github.com>
2020-04-13 21:53:11 -0500

    Revert "Merge branch 'UI_Work_(MostUpToDate)'"
    This reverts commit b30e225f4421f796285075b6e6ea985dded53dd4, reversing
    changes made to d712fbdbd78a6cfb69e7761fbedab793b68e2eb5.

--- Assets/Scripts/TakeScreenshotScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TakeScreenshotScript : MonoBehaviour
-{
-    // This script will take a screenshot of the 
- 
-}
