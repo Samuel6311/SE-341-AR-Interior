4b8b3a07e6885ae9dc4874f688741646bab82894
Hissamuddin Shaikh <57640329+HissamShk@users.noreply.github.com>
2020-04-13 22:41:07 -0500

    Master Update
    The Master is now up to date based on our progress.
    The following still needs to be addressed:
    - Save and Load need to be refined
    - Models turned to prefabs, and hooked up.
    - Material need to be set.

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+    public GameObject MenuObject;
+    public GameObject MenuObject2;
+    public GameObject MenuObject3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+
+        MenuObject.SetActive(true);
+           
+    }
+
+    public void CloseMenu()
+    {
+        MenuObject.SetActive(false);
+        if (MenuObject2 != null)
+        {
+            MenuObject2.SetActive(false);
+            if (MenuObject3 != null)
+                MenuObject3.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/TakeScreenshotScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshotScript : MonoBehaviour
+{
+    // This script will take a screenshot of the 
+ 
+}
