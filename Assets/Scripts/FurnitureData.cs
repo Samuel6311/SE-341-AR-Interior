6553e4ae3a80d830e0c7622ae55926c98f904bb8
mkr64294 <mark-rice@bethel.edu>
2020-04-23 23:25:59 -0500

    I think We're Done
    Competed

---
+++ Assets/Scripts/FurnitureData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+[System.Serializable]
+public class FurnitureData
+{
+    public List<OneFurnitureData> FurnitureList;
+
+    public FurnitureData()
+    {
+        FurnitureList = new List<OneFurnitureData>();
+
+
+    }
+
+
+}
+
+
